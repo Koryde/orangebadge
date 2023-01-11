40728bf8794a92ee5b248f697a36744eb49cce74
Koryde <94546744+Koryde@users.noreply.github.com>
2023-01-11 10:59:17 +0100

    c1rsetaogommitt

---
+++ OITNB/OITNBApp.swift
@@ -0,0 +1,17 @@
+//
+//  OITNBApp.swift
+//  OITNB
+//
+//  Created by Roberto D'Anna on 11/01/23.
+//
+
+import SwiftUI
+
+@main
+struct OITNBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
