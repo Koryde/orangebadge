98642297a623a4a48ae26b1adc07c80b8937d3d8
Raffaele S <coverforhobbyit@gmail.com>
2023-01-11 12:45:59 +0100

    Gang

--- OITNB/ContentView.swift
+++ OITNB/ContentView.swift
@@ -8,12 +8,16 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var bac : String = ""
+    @EnvironmentObject var appData : AppData
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("BAC")
+                .fontWeight(.bold)
+                .font(.system(size: 32))
+            Text(bac.prefix(5))
+        }.onAppear{
+            bac = String(appData.calculateBac(drink: Drink(category: .shot, name: "Negroni", alcoholByVolume: 24, milliliters: 90.0, iconName: ""), myWeight: 80, myGender: "Male", haveEat: true))
         }
         .padding()
     }
