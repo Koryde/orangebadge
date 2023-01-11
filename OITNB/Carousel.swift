f0f34a3184d5be3fd922ebe6c98294d750697514
Andrea <andrea.masturzo@icloud.com>
2023-01-11 12:50:23 +0100

    Carousel final commit

---
+++ OITNB/Carousel.swift
@@ -0,0 +1,55 @@
+//
+//  Carousel.swift
+//  OITNB
+//
+//  Created by Andrea Masturzo on 11/01/23.
+//
+
+import SwiftUI
+
+struct Carousel<Content: View>: View {
+    let pageWidth: CGFloat
+    let content: [Content]
+    
+    @State private var offset: CGFloat = 0
+    @State private var index = 0
+    
+    var body: some View {
+        ScrollView(.horizontal, showsIndicators: false) {
+            HStack(alignment: .center, spacing: 0) {
+                ForEach(content.indices) { index in
+                    self.content[index]
+                        .frame(width: self.pageWidth, height: nil)
+                }
+            }
+        }
+        .content.offset(x: offset)
+        .frame(width: pageWidth, height: nil, alignment: .leading)
+        .gesture(
+            DragGesture()
+                .onChanged { value in
+                    self.offset = value.translation.width - (CGFloat(self.index) * self.pageWidth)
+                }
+                .onEnded { value in
+                    let newIndex = self.newIndex(-value.translation.width, currentIndex: self.index)
+                    self.index = newIndex
+                    withAnimation { self.offset = -(CGFloat(self.index) * self.pageWidth) }
+                }
+        )
+    }
+    
+    private func newIndex(_ translation: CGFloat, currentIndex: Int) -> Int {
+        let snapDistance = pageWidth / 2
+        let newIndex: Int
+        if translation > -snapDistance {
+            newIndex = currentIndex + 1 >= content.count ? 0 : currentIndex + 1
+        } else if translation < snapDistance {
+            newIndex = currentIndex - 1 < 0 ? content.count - 1 : currentIndex - 1
+        } else {
+            newIndex = currentIndex
+        }
+        return newIndex
+    }
+}
+
+
