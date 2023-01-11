28441a2955dfc05741ad657fb07af4d556ab1e58
Fabriziochiariello7 <113529227+Fabriziochiariello7@users.noreply.github.com>
2023-01-11 12:24:44 +0100

    BasicDrinksView

---
+++ OITNB/MainScreenView.swift
@@ -0,0 +1,26 @@
+//
+//  MainScreenView.swift
+//  OITNB
+//
+//  Created by Fabrizio Chiariello on 11/01/23.
+//
+
+import SwiftUI
+
+struct MainScreenView: View {
+   
+    
+    var body: some View {
+    
+        BasicDrinksView()
+        
+    }
+}
+
+struct MainScreenView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainScreenView()
+    }
+}
+
+
