af9f41ef312352092663ea02752fd7841f1c8751
Fabriziochiariello7 <113529227+Fabriziochiariello7@users.noreply.github.com>
2023-01-13 12:20:54 +0100

    Drink list first commit

--- OITNB/Model/Model.swift
+++ OITNB/Model/Model.swift
@@ -25,7 +25,15 @@ struct Drink : Identifiable, Hashable {
 class AppData : ObservableObject {
     
     // MARK: List of Drinks
-    @Published var allDrinks : [Drink] = []
+    @Published var allDrinks : [Drink] =
+[Drink(category: .lightCocktail,name: "Mimosa",alcoholByVolume: 12.0,milliliters: 150.0,iconName: ""),
+ Drink(category: .lightCocktail,name: "Aperol Spritz",alcoholByVolume: 11.0,milliliters: 150.0,iconName: ""),
+ Drink(category: .lightCocktail, name: "Moscow Mule", alcoholByVolume: 12.0, milliliters: 175.0, iconName: ""),
+ Drink(category: .lightCocktail, name: "Cuba Libre", alcoholByVolume: 12.0, milliliters: 180.0, iconName: ""),
+ Drink(category: .lightCocktail, name: "Pina Colada", alcoholByVolume: 17.0, milliliters: 130.0, iconName: ""),
+ Drink(category: .lightCocktail, name: "Gin Tonic", alcoholByVolume: 17.0, milliliters: 140.0, iconName: ""),
+ Drink(category: .lightCocktail, name: "Mojito", alcoholByVolume: 18.0, milliliters: 180.0, iconName: "")
+]
     
     // MARK: Functions
     func calculateBac(drink: Drink, myWeight: Double, myGender: String, haveEat: Bool, bacValue: String) -> Double {
