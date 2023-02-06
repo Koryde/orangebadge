34783a1895a9879c89dca1c132604c0e23c97a85
Fabriziochiariello7 <113529227+Fabriziochiariello7@users.noreply.github.com>
2023-02-06 12:51:52 +0100

    Info settings

--- OITNB/View/ContentView/AccountView.swift
+++ OITNB/View/ContentView/AccountView.swift
@@ -8,20 +8,38 @@
 import SwiftUI
 
 struct AccountView: View {
+    @State var gender = ["Male", "Female", "Prefer not to say"]
+    @State var selectedGender = "Male"
+    @State var weight = Array(stride(from: 35, to: 200, by: 1))
+    @State var selectedWeight = 65
+    
     var body: some View {
         NavigationView{
             Form{
                 Section(header: Text("My info")){
-//                    Picker()a
+                    VStack{
+                        Picker("Gender", selection: $selectedGender) {
+                            ForEach(gender, id: \.self) { _gender in
+                                Text("\(_gender)")
+                            }
+                            
+                        }
+                        Picker("Weight", selection: $selectedWeight){
+                            ForEach(weight, id: \.self) { _weight in
+                                Text("\(_weight)")
+                                
+                            }
+                        }
+                    }
                 }
             }
+            
         }
-        
     }
-}
-
-struct AccountView_Previews: PreviewProvider {
-    static var previews: some View {
-        AccountView()
+    
+    struct AccountView_Previews: PreviewProvider {
+        static var previews: some View {
+            AccountView()
+        }
     }
 }
